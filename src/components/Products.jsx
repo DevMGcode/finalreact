60d29235a6ccadeebcd7168e27073a95b7841262
MeliiG <meli.bogar15@gmail.com>
2023-10-25 12:01:41 -0500

    mejora

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,42 +1,28 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './css/Products.css' 
-
+import './css/Products.css';
 
 const Products = () => {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    axios('https://fakestoreapi.com/products').then((res) => setData(res.data));
-  }, [])
+    useEffect(() => {
+        axios('https://fakestoreapi.com/products').then((res) => setData(res.data));
+    }, []);
 
-  const totalItems = async() =>{ 
-     console.log(data.length); // total de productos
-}
-totalItems()
-  const promedio = async() =>{
-    const sum = data.reduce((accumulator, item) => accumulator + item.price, 0);
-    const average = sum / data.length;
-    console.log('Promedio:', average);
-  }
-  promedio()
-
-  return data.map((product) => {
     return (
-      <div className="contenedor" key={product.id}>
-        <div className="card"  style={{ width: '25rem' }}>
-          <img src={`${product.image}`} style={{ width: '25rem' }} alt="img-product-card" />
-          <h3>{product.title}</h3>
-          <h4>${product.price}</h4>
-          <button>Comprar</button>
+        <div className="container1">
+            <div className="product-card-container">
+                {data.map((product) => (
+                    <div className="card" key={product.id}>
+                        <img src={`${product.image}`} alt="img-product-card" />
+                        <h3>{product.title}</h3>
+                        <h4>${product.price}</h4>
+                        <button>Comprar</button>
+                    </div>
+                ))}
+            </div>
         </div>
-      </div>
-
-      
     );
-  }
-  
-  )
-}
+};
 
-export default Products
+export default Products;
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,63 +1,71 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import TotalProductos from "./TotalProductos";
-import { useState, useEffect } from "react";
 import PrecioPromedio from "./PrecioPromedio";
 import TotalPedidos from "./Totalpedidos";
 import MasVendidos from "./Masvendidos";
-import axios from "axios";
 import IngresosTotales from "./IngresosTotales";
-import  ingreso  from "../css/img/ingreso.png";
-import  productos  from "../css/img/productos.png";
-import  promedio  from "../css/img/promedio.png";
-import  orden  from "../css/img/orden.png";
-
-
-
+import ingreso from "../css/img/ingreso.png";
+import productos from "../css/img/productos.png";
+import promedio from "../css/img/promedio.png";
+import orden from "../css/img/orden.png";
+// import "../css/Spinner.css"; // Asegúrate de importar el archivo CSS del spinner
 
 const Dashboard = (props) => {
   const [dataProducts, setDataProducts] = useState([]);
   const [dataCart, setDataCart] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const data = props;
 
-  async function conectAPI(endpoint, setData) {
-    await axios(`${data.API}/${endpoint}`)
-      .then((res) => setData(res.data))
-      .catch((err) => console.error(err));
+  async function connectAPI(endpoint, setData) {
+    try {
+      const response = await axios.get(`${data.API}/${endpoint}`);
+      // Introduce un retardo de 5 segundos antes de establecer los datos y desactivar el spinner
+      setTimeout(() => {
+        setData(response.data);
+        setLoading(false);
+      }, 5000);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
   }
+
   useEffect(() => {
-    conectAPI("products", setDataProducts);
+    connectAPI("products", setDataProducts);
   }, []);
 
   useEffect(() => {
-    conectAPI("carts", setDataCart);
+    connectAPI("carts", setDataCart);
   }, []);
-
-
-  //grafico de torta 
-
-  
-
 
   return (
-
     <div className="dashboard">
-      <div className="contenedor2">
-      <img src={ingreso} className="tamañoimg" alt="" />
-      <div className="item">
-        <IngresosTotales data={data} />
-      </div>
-      <img src={productos} className="tamañoimg" alt="" />
-      <div className="item" >        
-        <TotalProductos dataProducts={dataProducts} />
-      </div>
-      <img src={promedio} className="tamañoimg" alt="" />
-      <div className="item">
-        <PrecioPromedio dataProducts={dataProducts} />
-      </div>
-      <img src={orden} className="tamañoimg" alt="" />
-      <div className="item">
-        <TotalPedidos dataCart={dataCart} />
-      </div>
-      </div>
+      {loading ? (
+        <div className="spinner-container">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <div className="contenedor2">
+          <img src={ingreso} className="tamañoimg" alt="" />
+          <div className="item">
+            <IngresosTotales data={data} />
+          </div>
+          <img src={productos} className="tamañoimg" alt="" />
+          <div className="item">
+            <TotalProductos dataProducts={dataProducts} />
+          </div>
+          <img src={promedio} className="tamañoimg" alt="" />
+          <div className="item">
+            <PrecioPromedio dataProducts={dataProducts} />
+          </div>
+          <img src={orden} className="tamañoimg" alt="" />
+          <div className="item">
+            <TotalPedidos dataCart={dataCart} />
+          </div>
+        </div>
+      )}
       <div className="item-sell">
         <MasVendidos dataCart={dataCart} />
       </div>
@@ -66,3 +74,4 @@ const Dashboard = (props) => {
 };
 
 export default Dashboard;
+
