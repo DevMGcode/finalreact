e988f38f69ff0710950cd0d1ae39cb28a4fb1d0e
DevMGcode <meli.bogar15@gmail.com>
2024-06-26 12:25:00 -0500

    modify2

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Carousel } from 'react-bootstrap';
-import {imag1} from '../src/assets/img/banner1.png';
-import {imag2} from '../src/assets/img/banner2.png';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import banner1 from '../src/assets/img/banner1.png';
+import banner2 from '../src/assets/img/banner2.png';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  
-
     <App />
-       {/* Slider (Carousel) */}
-       <Carousel fade>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={imag1}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>Bienvenido a nuestro sitio</h3>
-            <p>Descubre nuestras ofertas exclusivas</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={imag2}
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Explora nuestra gama de productos</h3>
-            <p>Encuentra lo que necesitas al mejor precio</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        
-      </Carousel>
-  </React.StrictMode>,
-)
+    {/* Slider (Carousel) */}
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={banner1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Bienvenido a nuestro sitio</h3>
+          <p>Descubre nuestras ofertas exclusivas</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={banner2}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Explora nuestra gama de productos</h3>
+          <p>Encuentra lo que necesitas al mejor precio</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  </React.StrictMode>
+);
